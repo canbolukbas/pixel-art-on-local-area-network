1afa34cc107c4e50b5cbc1610a5b7411da3e81cb
can <canbolukbas98@gmail.com>
2022-01-20 17:35:02 +0300

    update server to be a chat server

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 import socket
+import queue
+from threading import Thread
 
 PACKET_SIZE = 1024
 TCP_PORT = 12345
+OUTGOING_MESSAGES = queue.Queue()
 
 # returns local IP.
 def get_my_ip():
@@ -13,6 +16,14 @@ def get_my_ip():
         s.close()
     return ip_address
 
+def chat():
+    global OUTGOING_MESSAGES  # to be able to update global variable.
+    
+    print("Chat started! (Type exit to exit)\n")
+    while True:
+        message = input()
+        OUTGOING_MESSAGES.put(message)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     my_ip = get_my_ip()
     s.bind((my_ip, TCP_PORT))
@@ -20,6 +31,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         client_socket, address = s.accept()
         print("Connection from {} has been accomplished!".format(address))
+        chat_io = Thread(target=chat, daemon=True)
+        chat_io.start()
 
-        client_socket.send(bytes("Naber la essek... :D", "utf-8"))
+        while True:
+            if OUTGOING_MESSAGES.empty():
+                # wait for IO
+                continue
+
+            text = OUTGOING_MESSAGES.get()
+            if text == "exit":
+                break
+            client_socket.send(bytes(text, "utf-8"))
+        
         client_socket.close()
